13f47bee21c7476ad530edfb77f2fd29ad17beae
miladimos <milad.jafari6210@gmail.com>
2023-02-15 19:15:53 +0330

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float gravity = -9.8f;
+    public float strength = 5f;
+
+    private Vector3 direction;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strength;
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                direction = Vector3.up * strength;
+            }
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+    }
+}
